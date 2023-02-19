d67285c12e9a2f686af82ae1ed09d436789c1aa1
KombinatorVit <slonik0775@gmail.com>
2023-02-19 22:46:13 +0200

    update styles and EditPizza

--- src/components/SinglePizza.tsx
+++ src/components/SinglePizza.tsx
@@ -1,5 +1,8 @@
-import React, {FC} from 'react';
-import Pizza from "../models/Pizza";
+import React, { FC, useState } from 'react';
+import EditPizzaForm from './EditPizzaForm';
+import Pizza from '../models/Pizza';
+import {AiFillDelete, AiFillEdit} from "react-icons/ai";
+
 
 interface SinglePizzaProps {
     pizza: Pizza;
@@ -7,12 +10,41 @@ interface SinglePizzaProps {
     deletePizza: (id: number) => void;
 }
 
-const SinglePizza: FC<SinglePizzaProps> = () => {
-    return (
-        <div>
+const SinglePizza: FC<SinglePizzaProps> =
+    ({ pizza, updatePizza, deletePizza }) => {
+        const [edit, setEdit] = useState<boolean>(false);
 
-        </div>
-    );
-};
+        const handleToggleEdit = () => {
+            setEdit(!edit);
+        }
+
+        const handleDelete = () => {
+            deletePizza(pizza.id);
+        }
+
+
+        return (
+            <div className="pizza">
+
+                <img src={`/images/${pizza.img}`} alt={pizza.title}/>
+                <h2>{pizza.title}</h2>
+                <span>{pizza.price} ₽</span>
+
+                <div className="pizza-controls">
+                    <AiFillEdit onClick={handleToggleEdit}/>
+                    <AiFillDelete onClick={handleDelete} />
+                </div>
+
+                {edit
+                    ? <EditPizzaForm
+                        data={pizza}
+                        updatePizza={updatePizza}
+                        handleToggleEdit={handleToggleEdit}
+                    />
+                    : null}
+
+            </div>
+        )
+    }
 
 export default SinglePizza;
